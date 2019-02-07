d8119dbdd350addce7d8ed3c5fd871e8910eea36
OliverOng1995 <44562019+OliverOng1995@users.noreply.github.com>
2019-02-07 16:59:22 +0800

    Update UnityAds2.js

--- www/UnityAds2.js
+++ www/UnityAds2.js
@@ -31,7 +31,7 @@ exports.ShowVideoAd = function(videoAdPlacementId, fn) {
        var resultArray=JSON.parse(str)
         console.log(resultArray[1]);
         if(resultArray[1]="SHOWING"){
-          self.onVideoShow();
+          self.onVideoShow(resultArray[0]);
         }
       // if(resultArray[1]="READY" or resultArray[1]="NOT_READY"){
              fn(null, str);
