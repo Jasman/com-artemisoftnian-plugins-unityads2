1f2f1e2237684c05117473578ca36bdf1153e024
OliverOng1995 <44562019+OliverOng1995@users.noreply.github.com>
2019-02-07 14:20:12 +0800

    Update UnityAds2.js

--- www/UnityAds2.js
+++ www/UnityAds2.js
@@ -29,14 +29,12 @@ exports.ShowVideoAd = function(videoAdPlacementId, fn) {
     function success (str){
        var resultArray=JSON.parse(str)
         console.log(resultArray[1]);
-       // if(resultArray[1]="READY"){
-            
-           // this.onVideoShowReady(videoAdPlacementId);
-        //}
-       // else if(resultArray[1]="SHOWING"){
-         //    this.onVideoShow(videoAdPlacementId);
-       // }
-       // fn(null, str);
+        if(resultArray[1]="SHOWING"){
+             this.onVideoShow(videoAdPlacementId);
+       }
+       if(resultArray[1]="READY" or resultArray[1]="NOT_READY"){
+             fn(null, str);
+       }
     }
     function error (str){
         fn(str, null);
